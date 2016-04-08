3faa9481fe9ddb83582d65c798be2887de1c9e5b
ingcesarvelasquez <cesarandresvelasquez01@gmail.com>
2016-04-08 17:23:49 -0500

    Commit 6 vista modificada

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
 
 #https://docs.djangoproject.com/en/1.8/topics/http/views/
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+#https://docs.djangoproject.com/en/1.8/ref/models/querysets/
+	post = Post.objects.filter(published_date_lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
+
