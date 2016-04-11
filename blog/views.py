3d71d6435fd06ea2b4c4a6beb235a3c63981c372
ingcesarvelasquez <cesarandresvelasquez01@gmail.com>
2016-04-11 09:37:22 -0500

    Agregando archivos estaticos css y embelleciendo el sitio

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,6 @@ from blog.models import Post
 #https://docs.djangoproject.com/en/1.8/topics/http/views/
 def post_list(request):
 #https://docs.djangoproject.com/en/1.8/ref/models/querysets/
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
