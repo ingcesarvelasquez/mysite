14073aa4d2ea04b6389fdbf09f7bc2d4955f178a
ingcesarvelasquez <cesarandresvelasquez01@gmail.com>
2016-04-11 10:19:09 -0500

    Commit 11: Agregando url, views y template par ver cada posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from blog import views
-
+#https://docs.djangoproject.com/en/1.8/topics/http/urls/
 urlpatterns = [
 	url(r'^$', views.post_list),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
